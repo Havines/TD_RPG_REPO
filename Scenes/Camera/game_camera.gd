f509017155fc8b4a23e9d115145b46f0220c901d
Havines <55022144+Havines@users.noreply.github.com>
2023-09-21 02:30:02 +0500

    add 32x32 graphic

--- Scenes/Camera/game_camera.gd
+++ Scenes/Camera/game_camera.gd
@@ -6,10 +6,27 @@ var target_pos = Vector2.ZERO
 
 func _ready():
 	make_current()
+	init_limit()
 
+
+func init_limit():
+	limit_bottom = 0
+	limit_left = 0
+	limit_right = 0
+	limit_top = 0
+	
+	var tilemap = get_node("../TileMap") as TileMap
+	
+	var used = tilemap.get_used_rect()
+	limit_left = min(used.position.x * 32.0, limit_left)
+	limit_right = max(used.end.x * 32.0, limit_right)
+	limit_bottom = max(used.end.y * 32.0, limit_bottom)
+	limit_top = min(used.position.y * 32.0, limit_top)
 
 
 func _process(delta):
 	if player_node.size() > 0:
 		target_pos = player_node[0].global_position
-	global_position = global_position.lerp(target_pos, 1.0 - exp(-delta * 20))
+	else:
+		return
+	global_position = target_pos
